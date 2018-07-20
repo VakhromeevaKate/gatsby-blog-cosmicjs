b924dc69f365314cec73ce141f58018afaedfded
Ekaterina Vakhromeeva <vakhromeevae@gmail.com>
2018-07-20 14:57:17 +0300

    gatsby-config changed from default

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+    plugins: [
+        `gatsby-plugin-offline`,
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/pages`,
+                name: 'pages',
+            },
+        },
+        {
+            resolve: 'gatsby-source-cosmicjs',
+            options: {
+                bucketSlug: 'gatsby-blog-cosmic-js', // Bucket Slug
+                objectTypes: ['posts','settings'], // List of the Object Types you want to be able to request from Gatsby.
+                apiAccess: {
+                    read_key: '',
+                }
+            }
+        },
+    ],
 }
